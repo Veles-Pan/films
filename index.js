a69a8d58114fe480011128095bb9724aca39d783
Veles-Pan <73409754+Veles-Pan@users.noreply.github.com>
2021-02-01 16:58:25 +0300

    Переписан код сортировки

--- index.js
+++ index.js
@@ -1,36 +1,122 @@
-import * as filmsJson from './flm.json';
+import filmsJson from './flm.json';
 
-let filmsList = Object.values(filmsJson);
+let initialFilmsList = Object.values(filmsJson);
 
-filmsList.splice(-1,1);
+var navigationElement = null;
 
 var newTable = document.createElement("table");
 newTable.className = "filmsTable";
 
-function createTable(checkTable){
+document.body.onload = onLoad;
+var elementNavigation = null;
+
+function sortFilmsByName(filmsList) {
+  return filmsList.sort(function(a, b){
+    if(a.Name_eng < b.Name_eng) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+}
+
+function sortFilmsByRating(filmsList) {
+  return filmsList.sort((a, b) => b.Rating - a.Rating);
+}
+
+function sortFilmsByYear(filmsList) {
+  return filmsList.sort((a, b) => b.Year - a.Year);
+}
+
+function createOption({ value, label, select }) {
+  const option = document.createElement('option');
+  option.value = value;
+  option.innerHTML = label
+  select.appendChild(option);
+}
+function onLoad() {
+  console.log(initialFilmsList);
+  var divForTable = document.createElement("div");
+  divForTable.className = "tableDiv"
+  divForTable.id = "tableDiv";
+
+  divForTable.appendChild(newTable)
+
+  elementNavigation = document.getElementById("footer");
+  document.body.insertBefore(divForTable, elementNavigation);
+  var newSelect = document.createElement('select');
+  newSelect.className = "selectFunc";
+  newSelect.id = 'selectId'
+
+  createOption({ value: "sortRating", label: 'Сортировать по рейтингу (убывание)', select: newSelect });
+  createOption({ value: "sortRatingReverse", label: 'Сортировать по рейтингу (возрастание)', select: newSelect });
+  createOption({ value: "sortYear", label: 'Сортировать по году (убывание)', select: newSelect });
+  createOption({ value: "sortYearReverse", label: 'Сортировать по году (возрастание)', select: newSelect });
+  createOption({ value: "sortName", label: 'Сортировать по алфавиту (A-Z)', select: newSelect });
+  createOption({ value: "sortNameReverse", label: 'Сортировать по алфавиту (Z-A)', select: newSelect });
+
+  var divForSelect = document.createElement("div");
+  divForSelect.className = "mySelect"
+
+  divForSelect.appendChild(newSelect)
+  navigationElement = document.getElementById("tableDiv");
+  document.body.insertBefore(divForSelect, navigationElement);
+
+  newSelect.addEventListener("change", function(e) {
+    const filmsList = initialFilmsList.slice();
+    switch (e.target.value) {
+      case 'sortRating': {
+        const filmsListSorted = sortFilmsByRating(filmsList);
+        createTable(true, filmsListSorted);
+        break;
+      }
+      case 'sortYear': {
+        const filmsListSorted = sortFilmsByYear(filmsList);
+        createTable(true, filmsListSorted);
+        break;
+      }
+      case 'sortName': {
+        const filmsListSorted = sortFilmsByName(filmsList)
+        createTable(true, filmsListSorted);
+        break;
+      }
+      case 'sortRatingReverse': {
+        const filmsListSorted = sortFilmsByRating(filmsList).reverse();
+        createTable(true, filmsListSorted);
+        break;
+      }
+      case 'sortYearReverse': {
+        const filmsListSorted = sortFilmsByYear(filmsList).reverse();
+        createTable(true, filmsListSorted);
+        break
+      }
+      case 'sortNameReverse': {
+        const filmsListSorted = sortFilmsByName(filmsList).reverse();
+        createTable(true, filmsListSorted);
+      }
+    }
+  });
+}
+
+function createCell({ tr, classname, value }) {
+  let td = document.createElement('td');
+  td.className = classname;
+  td.innerHTML = value;
+  tr.appendChild(td);
+}
+
+function createTable(checkTable, filmsList) {
 
   var tableUpdate = document.createElement("table");
   tableUpdate.className = "filmsTable";
 
   for (let film of filmsList) {
-        
-    let tr = document.createElement('tr');
+    var tr = document.createElement('tr');
     tr.className = "thisIsTr";
-            
-    let td1 = document.createElement('td');
-    td1.className = "filmName";
-    td1.innerHTML = film.Name_eng;
-    tr.appendChild(td1);
-            
-    let td2 = document.createElement('td');
-    td2.className = "filmRating";
-    td2.innerHTML = film.Rating;
-    tr.appendChild(td2);
-            
-    let td3 = document.createElement('td');
-    td3.className = "filmYear";
-    td3.innerHTML = film.Year;
-    tr.appendChild(td3);
+
+    createCell({ tr: tr, classname: 'filmName', value: film.Name_eng })
+    createCell({ tr: tr, classname: 'filmRating', value: film.Rating })
+    createCell({ tr: tr, classname: 'filmYear', value: film.Year })
 
     let td4 = document.createElement('td');
     let imageUrl = document.createElement('img');
@@ -39,127 +125,23 @@ function createTable(checkTable){
     imageUrl.alt = film.Name_rus;
     td4.className = "filmImg";
     td4.appendChild(imageUrl);
+
     tr.appendChild(td4);
 
-    if (checkTable == false) {
+    if (checkTable === false) {
       newTable.appendChild(tr);
-
-      document.body.onload = addElement;
-      var elementNavigation = null;
-
-      function addElement() {
-
-        var divForTable = document.createElement("div");
-        divForTable.className = "tableDiv"
-        divForTable.id = "tableDiv";
-      
-        divForTable.appendChild(newTable)
-          
-        elementNavigation = document.getElementById("footer");
-        document.body.insertBefore(divForTable, elementNavigation);
-
-      };
     } else {
-      tableUpdate.appendChild(tr); 
-    };
-  };
-  if (checkTable == true) {
+      tableUpdate.appendChild(tr);
+    }
+  }
+  if (checkTable === true) {
     var elementForChange = document.getElementsByClassName("tableDiv")[0];
 
-    var tableForDelete = elementForChange.firstChild; 
+    var tableForDelete = elementForChange.firstChild;
 
     elementForChange.replaceChild(tableUpdate, tableForDelete);
-  };
-};
+  }
+}
 
-createTable(false);
+createTable(false, initialFilmsList);
 
-var navigationElement = null;
-var divForSelect = null;
-
-var newSelect = document.createElement('select');
-newSelect.className = "selestFunc";
-
-var firstOption = document.createElement('option');
-firstOption.value = "sortRating";
-firstOption.innerHTML = "Сортировать по рейтингу (убывание)"
-
-newSelect.appendChild(firstOption);
-
-var secondOption = document.createElement('option');
-secondOption.value = "sortRatingReverse";
-secondOption.innerHTML = "Сортировать по рейтингу (возрастание)"
-
-newSelect.appendChild(secondOption);
-
-var thirdOption = document.createElement('option');
-thirdOption.value = "sortYear";
-thirdOption.innerHTML = "Сортировать по году (убывание)"
-
-newSelect.appendChild(thirdOption);
-
-var fourthOption = document.createElement('option');
-fourthOption.value = "sortYearReverse";
-fourthOption.innerHTML = "Сортировать по году (возрастание"
-
-newSelect.appendChild(fourthOption);
-
-var fifthOption = document.createElement('option');
-fifthOption.value = "sortName";
-fifthOption.innerHTML = "Сортировать по алфавиту (A-Z)"
-
-newSelect.appendChild(fifthOption);
-
-var sixthOption = document.createElement('option');
-sixthOption.value = "sortNameReverse";
-sixthOption.innerHTML = "Сортировать по алфавиту (Z-A)"
-
-newSelect.appendChild(sixthOption);
-
-var divForSelect = document.createElement("div");
-divForSelect.className = "mySelect"
-
-divForSelect.appendChild(newSelect)
-  
-navigationElement = document.getElementById("tableDiv");
-document.body.insertBefore(divForSelect, navigationElement);
-
-newSelect.addEventListener("change", function() {
-  if (newSelect.value == 'sortRating') {
-    (function () {
-      filmsList.sort((a, b) => b.Rating - a.Rating);
-      createTable(true);
-      }());
-  } else if (newSelect.value == "sortYear") {
-    (function () {
-      filmsList.sort((a, b) => b.Year - a.Year);
-      createTable(true);
-      }());
-  } else if (newSelect.value == "sortName") {
-    (function () {
-      filmsList.sort(function(a, b){
-        if(a.Name_eng < b.Name_eng) return -1;
-        if(a.Name_eng > b.Name_eng) return 1;
-      });
-      createTable(true);
-      }());
-  } else if (newSelect.value == "sortRatingReverse") {
-    (function () {
-      filmsList.sort((a, b) => a.Rating - b.Rating);
-      createTable(true);
-      }());
-  } else if (newSelect.value == "sortYearReverse") {
-    (function () {
-      filmsList.sort((a, b) => a.Year - b.Year);
-      createTable(true);
-      }());
-  } else if (newSelect.value == "sortNameReverse") {
-    (function () {
-      filmsList.sort(function(a, b){
-        if(a.Name_eng < b.Name_eng) return 1;
-        if(a.Name_eng > b.Name_eng) return -1;
-      });
-      createTable(true);
-      }());
-  };
-});
